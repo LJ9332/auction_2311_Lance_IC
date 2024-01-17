d0f8be7efd735cfb45265268c57d1c67bf62618c
lance butler <lancej2004@gmail.com>
2024-01-17 10:28:00 -0700

    Test: Creates test for add_bid and highest_bid

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -21,4 +21,24 @@ RSpec.describe Item do
             expect(@item2.name).to eq("Bamboo Picture Frame")
         end
     end
+
+    describe '#add_item' do
+        before(:each) do
+            @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+            @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+        end
+
+        it "can add to bids to items hash" do
+            expect(@item1.bids).to eq({})
+            
+            @item1.add_bid(@attendee2, 20)
+            @item1.add_bid(@attendee1, 22)
+            
+            expect(@item1.bids).to eq({@attendee2 => 20, @attendee1 => 22})
+        end
+
+        xit "can identify highest bid" do
+            expect(@item1.current_high_bid).to eq(22)
+        end
+    end
 end
