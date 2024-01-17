b79b38b0d8a71d448af1af653e97b65ca2f2b3ff
lance butler <lancej2004@gmail.com>
2024-01-17 10:33:56 -0700

    Fix: Creates working test for current_high_bid

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -36,8 +36,11 @@ RSpec.describe Item do
             
             expect(@item1.bids).to eq({@attendee2 => 20, @attendee1 => 22})
         end
+        
+        it "can identify highest bid" do
+            @item1.add_bid(@attendee2, 20)
+            @item1.add_bid(@attendee1, 22)
 
-        xit "can identify highest bid" do
             expect(@item1.current_high_bid).to eq(22)
         end
     end
