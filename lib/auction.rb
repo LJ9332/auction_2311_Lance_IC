5af4514dd845608c891bf597c3291d24a0770752
lance butler <lancej2004@gmail.com>
2024-01-17 11:26:18 -0700

    Feat: makes methods for unpopular_items and potential_revenue

--- lib/auction.rb
+++ lib/auction.rb
@@ -12,4 +12,21 @@ class Auction
     def item_names
         @items.map(&:name)
     end
+
+    def unpopular_items
+        unpopular_items = []
+        #require 'pry' ; binding.pry
+        @items.each do |item|
+            unpopular_items << item if item.bids.empty?
+        end
+        unpopular_items
+    end
+
+    def potential_revenue
+        total_revenue = 0
+        @items.each do |item|
+          total_revenue += item.bids.values.sum
+        end
+        total_revenue
+      end
 end
